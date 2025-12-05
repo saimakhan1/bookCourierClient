c4797d8717ee2cb9702d2aec5c1d7c608708bc63
Saima Khan <saimakhancsedu@gmail.com>
2025-12-05 22:39:15 +0600

    setup1 done

---
+++ src/Routes/router.jsx
@@ -0,0 +1,33 @@
+import { createBrowserRouter } from "react-router";
+import RootLayout from "../Layout/RootLayout/RootLayout";
+import HomePage from "../Pages/Home/HomePage";
+import AuthLayout from "../Layout/AuthLayout/AuthLayout";
+import Login from "../Pages/Auth/Login/Login";
+import Register from "../Pages/Auth/Register/Register";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: RootLayout,
+    children: [
+      {
+        path: "/",
+        Component: HomePage,
+      },
+    ],
+  },
+  {
+    path: "/",
+    Component: AuthLayout,
+    children: [
+      {
+        path: "/login",
+        Component: Login,
+      },
+      {
+        path: "/register",
+        Component: Register,
+      },
+    ],
+  },
+]);
