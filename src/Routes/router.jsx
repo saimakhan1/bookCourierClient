3ac484bfc0392b13385810f4f63c96e73ce7e42c
Saima Khan <saimakhancsedu@gmail.com>
2025-12-06 13:38:07 +0600

    librarian page added

--- src/Routes/router.jsx
+++ src/Routes/router.jsx
@@ -4,6 +4,8 @@ import HomePage from "../Pages/Home/HomePage";
 import AuthLayout from "../Layout/AuthLayout/AuthLayout";
 import Login from "../Pages/Auth/Login/Login";
 import Register from "../Pages/Auth/Register/Register";
+import PrivateRoute from "./PrivateRoute";
+import Librarian from "../Pages/Librarian/Librarian";
 
 export const router = createBrowserRouter([
   {
@@ -13,6 +15,14 @@ export const router = createBrowserRouter([
       {
         path: "/",
         Component: HomePage,
+      },
+      {
+        path: "/librarian",
+        element: (
+          <PrivateRoute>
+            <Librarian></Librarian>
+          </PrivateRoute>
+        ),
       },
     ],
   },
